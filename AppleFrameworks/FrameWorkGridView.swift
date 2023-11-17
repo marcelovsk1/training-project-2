6983e4762674f7885f6327fe4a3be78676aee7ab
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-17 14:49:16 -0300

    creating frameworkgridview

---
+++ AppleFrameworks/FrameWorkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameWorkGridView.swift
+//  AppleFrameworks
+//
+//  Created by Marcelo Amaral Alves on 2023-11-17.
+//
+
+import SwiftUI
+
+struct FrameWorkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameWorkGridView()
+}
