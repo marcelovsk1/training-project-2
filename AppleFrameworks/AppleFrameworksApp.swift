3f8cfe8db4dde72a843463651d6a3d3ea01a7bb8
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-17 14:45:50 -0300

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Marcelo Amaral Alves on 2023-11-17.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
