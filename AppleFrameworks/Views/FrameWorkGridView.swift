20ae7029b8354f536cf5799f5ebe822021d7109d
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-21 06:59:42 -0300

    add some columns

--- AppleFrameworks/Views/FrameWorkGridView.swift
+++ AppleFrameworks/Views/FrameWorkGridView.swift
@@ -8,7 +8,14 @@
 import SwiftUI
 
 struct FrameWorkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible())]
+    
     var body: some View {
+        LazyVGrid(columns: columns, content: {
+            /*@START_MENU_TOKEN@*/Text("Placeholder")/*@END_MENU_TOKEN@*/
+            /*@START_MENU_TOKEN@*/Text("Placeholder")/*@END_MENU_TOKEN@*/
+        })
         VStack {
             FrameWorkTitleView(name: "App Clips", imageName: "app-clip")
             
