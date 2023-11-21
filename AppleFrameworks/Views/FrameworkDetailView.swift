ef58115049af32b008ff5b03469f52e1ea610a28
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-21 14:10:14 -0300

    creating frameworkdetailview

---
+++ AppleFrameworks/Views/FrameworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailView.swift
+//  AppleFrameworks
+//
+//  Created by Marcelo Amaral Alves on 2023-11-21.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailView()
+}
