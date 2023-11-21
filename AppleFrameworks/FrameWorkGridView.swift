c2f77d211fe35416ffd69d2908d7663eb03727fa
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-21 06:48:07 -0300

    working on

--- AppleFrameworks/FrameWorkGridView.swift
+++ AppleFrameworks/FrameWorkGridView.swift
@@ -11,6 +11,14 @@ struct FrameWorkGridView: View {
     var body: some View {
         VStack {
             Image("app-clip")
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text("App Clips")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.5)
+            
         }
     }
 }
