1d3cd81cbcf273b4de58e9ebf385a762b5422af2
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-21 06:50:33 -0300

    working on

---
+++ AppleFrameworks/Model/FrameWorkTitleView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameWorkTitleView.swift
+//  AppleFrameworks
+//
+//  Created by Marcelo Amaral Alves on 2023-11-21.
+//
+
+import SwiftUI
+
+struct FrameWorkTitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameWorkTitleView()
+}
