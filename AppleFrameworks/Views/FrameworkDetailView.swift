34eb9e280a189d3f538451d7944e455cfa7f4c6a
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-21 14:33:13 -0300

    working on

--- AppleFrameworks/Views/FrameworkDetailView.swift
+++ AppleFrameworks/Views/FrameworkDetailView.swift
@@ -13,6 +13,20 @@ struct FrameworkDetailView: View {
     
     var body: some View {
         VStack {
+            HStack{
+                Spacer()
+                
+                Button {
+                
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .padding()
+            
             Spacer()
             
             FrameWorkTitleView(framework: framework)
