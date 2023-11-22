b6653879ee5d1433400e84d9d3565d8fb8947b80
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-22 14:49:57 -0300

    xdismissviewmodel is done

--- AppleFrameworks/Model/XDismissButtonViewModel.swift
+++ AppleFrameworks/Model/XDismissButtonViewModel.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct XDismissButtonViewModel: View {
+    
+    @Binding var isShowingDetailView: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack{
+                Spacer()
+                
+                Button {
+                    isShowingDetailView = false
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .padding()
+        }
     }
 }
 
 #Preview {
-    XDismissButtonViewModel()
+    XDismissButtonViewModel(isShowingDetailView: .constant(false))
 }
