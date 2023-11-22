25a0a9532a1bd4ca810a2f37295cef284cad70ed
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-22 14:48:14 -0300

    creating xdismissviewmodel

---
+++ AppleFrameworks/Model/XDismissButtonViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  XDismissButtonViewModel.swift
+//  AppleFrameworks
+//
+//  Created by Marcelo Amaral Alves on 2023-11-22.
+//
+
+import SwiftUI
+
+struct XDismissButtonViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    XDismissButtonViewModel()
+}
