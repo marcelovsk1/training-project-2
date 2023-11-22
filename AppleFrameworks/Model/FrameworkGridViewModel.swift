a98b9d9d9810d345f514e8a2697ee9577095d56a
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-22 11:19:54 -0300

    creating frameworkgridviewmodel

---
+++ AppleFrameworks/Model/FrameworkGridViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridViewModel.swift
+//  AppleFrameworks
+//
+//  Created by Marcelo Amaral Alves on 2023-11-22.
+//
+
+import SwiftUI
+
+struct FrameworkGridViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridViewModel()
+}
