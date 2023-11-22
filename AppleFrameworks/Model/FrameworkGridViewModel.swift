bb41114f4e778921e603980c93fded0150506ffb
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-22 13:33:23 -0300

    working on safariviewmodel

--- AppleFrameworks/Model/FrameworkGridViewModel.swift
+++ AppleFrameworks/Model/FrameworkGridViewModel.swift
@@ -15,6 +15,5 @@ final class FrameworkGridViewModel: ObservableObject {
         }
     }
     
-    
     @Published var isShowingDetailView = false
 }
--- AppleFrameworks/Model/SafariViewModel.swift
+++ AppleFrameworks/Model/SafariViewModel.swift
@@ -5,4 +5,17 @@
 //  Created by Marcelo Amaral Alves on 2023-11-22.
 //
 
-import Foundation
+import SwiftUI
+import SafariServices
+
+struct SafariViewModel: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariViewModel>) ->
+    SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    
+}
